fe05b378c77dd4b5530f4cd5471356699190a588
Samuel Gales Guimaraes <samgguimaraes@gmail.com>
2020-02-02 00:27:46 -0200

    first input handling

--- scripts/nave_player.gd
+++ scripts/nave_player.gd
@@ -1,14 +1,30 @@
 extends AnimatedSprite
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var moving = 0
+var speed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	self.set_process(true)
+	self.set_process_input(true)
 
 func _draw():
-	self.c
+	pass
+	
+func _process(delta):
+	if moving == 1:
+		self.global_translate(Vector2(speed * delta, 0))
+		self.frame = 1
+	elif moving == -1:
+		self.global_translate(Vector2(-speed * delta, 0))
+		self.frame = 2
+	else:
+		self.frame = 0
+
+func _input(event):
+	if(Input.is_key_pressed(KEY_RIGHT)):
+		moving = 1
+	elif(Input.is_key_pressed(KEY_LEFT)):
+		moving = -1
+	else:
+		moving = 0
